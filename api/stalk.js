86119b56b4a5b13425c8072b520de4658d890a75
ragzasoyyy <ragzasik@gmail.com>
2025-09-08 08:46:25 +0700

    Update stalk.js

--- api/stalk.js
+++ api/stalk.js
@@ -1,33 +1,33 @@
 // api/stalk.js
-import { getUserProfileInfo } from 'tiktok-scraper';
+import TikTokScraper from 'tiktok-scraper-lite';
 
 export default async function handler(req, res) {
-    const { username } = req.query;
-    if (!username) return res.status(400).json({ status: false, message: 'Username kosong' });
+  const { username } = req.query;
+  if (!username) return res.status(400).json({ status: false, message: 'Username kosong' });
 
-    try {
-        let cleanUsername = username.replace(/^https?:\/\/(www\.)?tiktok.com\/@/, '').split('?')[0];
-        const user = await getUserProfileInfo(cleanUsername);
+  try {
+    let cleanUsername = username.replace(/^https?:\/\/(www\.)?tiktok.com\/@/, '').split('?')[0];
+    const user = await TikTokScraper.getUserProfile(cleanUsername);
 
-        res.status(200).json({
-            status: true,
-            profile: {
-                nama: user.user.nickname,
-                namaPengguna: user.user.username,
-                deskripsiAkun: user.user.signature,
-                followers: user.user.fans,
-                following: user.user.following,
-                hearts: user.user.heart,
-                videoCount: user.user.videoCount,
-                verified: user.user.verified ? 'Verified' : 'Not Verified',
-                accountType: user.user.privateAccount ? 'Private Account' : 'Public Account',
-                created: user.user.createTime,
-                linkAkun: `https://www.tiktok.com/@${cleanUsername}`,
-                avatar: user.user.avatarLarger
-            }
-        });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ status: false, message: 'Gagal fetch data TikTok' });
-    }
+    res.status(200).json({
+      status: true,
+      profile: {
+        nama: user.nickname,
+        namaPengguna: user.username,
+        deskripsiAkun: user.signature,
+        followers: user.followerCount,
+        following: user.followingCount,
+        hearts: user.heartCount,
+        videoCount: user.videoCount,
+        verified: user.verified ? 'Verified' : 'Not Verified',
+        accountType: user.private ? 'Private Account' : 'Public Account',
+        created: user.createTime,
+        linkAkun: `https://www.tiktok.com/@${cleanUsername}`,
+        avatar: user.avatarLarger
+      }
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ status: false, message: 'Gagal fetch data TikTok' });
+  }
 }
